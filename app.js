0155782fb171184d029a727ddf7f455eff26c1a9
LeCrabe <gilles.gaubert@rocketmail.com>
2022-03-17 10:59:09 +0100

    Adapt code for heroku deployment

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+
+// lets use an env variable for heroku deployment
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
 const diveSiteRoutes = require('./routes/diveSiteRoutes');
 
 app.use(express.json());
@@ -7,4 +14,4 @@ app.use('/diveSite', diveSiteRoutes);
 app.use("/", (res, req) => {
     req.send("Welcome to Node API... Use /diveSite to proceed... Or not :-)")
 });
-app.listen(3000);
+app.listen(port);
